688a30e3ba2348ed1a80e99ba498d6a72d7291ee
Juliana <128743881+julianamirbosio@users.noreply.github.com>
2024-05-23 17:52:27 -0300

    Create addersubtractor.vhd

---
+++ addersubtractor.vhd
@@ -0,0 +1,16 @@
+library ieee;
+ieee.
+
+entity addersubtractor is
+	generic(	width: positive;
+				isAdder: boolean := false;
+				isSubtractor: boolean := false;
+				generateCout: boolean := false;
+				generateOvf: boolean := false;
+				fixedSecodOperand: integer := 0);
+	port(	
+		    a, b: in std_logic_vector(width-1 downto 0);
+			op: in std_logic;
+			result: out std_logic_vector(width-1 downto 0);
+			ovf, cout: out std_logic );
+end;
